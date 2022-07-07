addf264f87d24339a97a02e3aaafb03deb9865e6
dayoung100 <53906423+dayoung100@users.noreply.github.com>
2022-07-08 03:05:37 +0900

    Create 연습문제_평균구하기.cpp
    프로그래머스-연습문제-평균구하기 솔루션1 추가

---
+++ Programmers/연습문제_평균구하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    
+    double sum = 0;
+    
+    for(int a : arr){
+        sum += a;
+    }
+    
+    answer = sum / arr.size();
+    
+    return answer;
+}
