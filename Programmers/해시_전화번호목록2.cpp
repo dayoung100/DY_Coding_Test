6e0b442e1e83c7fe0304a345a8a75556add98f1b
dayoung100 <53906423+dayoung100@users.noreply.github.com>
2022-07-26 17:27:19 +0900

    Create 해시_전화번호목록2.cpp
    프로그래머스-해시-전화번호 목록 솔루션3 추가

---
+++ Programmers/해시_전화번호목록2.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    
+    unordered_map<string, int> m;
+    for(int p=0; p<phone_book.size(); p++) m[phone_book[p]] = 1;
+    
+    for(int i=0; i<phone_book.size();i++){
+        for(int j=0; j<phone_book[i].size(); j++){
+            string s = phone_book[i].substr(0,j+1);
+            if(m[s] && s != phone_book[i]){
+                return false;
+            }
+        }
+    }
+    
+    return true;
+}
