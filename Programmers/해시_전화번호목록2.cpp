1cc0e362266b0a32e8d78281721cb0826db513e6
dayoung100 <53906423+dayoung100@users.noreply.github.com>
2022-07-26 17:51:38 +0900

    Update 해시_전화번호목록2.cpp
    프로그래머스-해시-전화번호 목록 솔루션3 수정

--- Programmers/해시_전화번호목록2.cpp
+++ Programmers/해시_전화번호목록2.cpp
@@ -10,9 +10,9 @@ bool solution(vector<string> phone_book) {
     for(int p=0; p<phone_book.size(); p++) m[phone_book[p]] = 1;
     
     for(int i=0; i<phone_book.size();i++){
-        for(int j=0; j<phone_book[i].size(); j++){
+        for(int j=0; j<phone_book[i].size()-1; j++){
             string s = phone_book[i].substr(0,j+1);
-            if(m[s] && s != phone_book[i]){
+            if(m[s]){
                 return false;
             }
         }
