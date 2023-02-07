fe842980664970bfb38d4f95c929525ff5dcf218
dayounglee <dayoung015@gmail.com>
2023-02-07 21:29:42 +0900

    Create BJ15649.java
    백준 15649번 N과 M (1) 풀이 추가(java)

---
+++ BJ/BJ15649.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BJ15649 {
+	
+	static int N, R;
+	static int[] numbers;
+	static boolean[] visited;
+	
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		N = sc.nextInt();
+		R = sc.nextInt();
+		
+		numbers = new int[R];
+		visited = new boolean[N];
+		
+		perm(0);
+	}
+
+	private static void perm(int cnt) {
+		if(cnt == R) {
+			for(int j=0; j<R; j++) {
+				System.out.print(numbers[j]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			if(visited[i] == true) continue;
+			
+			numbers[cnt] = i+1;
+			visited[i] = true;
+			perm(cnt+1);
+			visited[i] = false;
+		}
+		
+	}
+}
