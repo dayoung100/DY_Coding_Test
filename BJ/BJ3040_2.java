86678d164cf02838706563ad2c91c4b4309ac733
dayounglee <dayoung015@gmail.com>
2023-02-08 22:19:51 +0900

    Create BJ3040_2.java
    backjoon 3040 백설 공주와 일곱 난쟁이 with java_2 used 'loop'

---
+++ BJ/BJ3040_2.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BJ3040_2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] data = new int[9];
+		int sum = 0;
+		
+		for(int i=0; i<data.length; i++){
+			data[i] = sc.nextInt();
+			sum += data[i]; //9명 난쟁이에 대한 수의 합
+		}
+		
+		Arrays.sort(data);
+		
+		//조합-반복문 이용
+		for (int i = 0; i < 8; i++) {
+			for(int j=i+1; j<9; j++) {
+				if(sum - data[i] - data[j] == 100) {
+					for(int k=0; k<9; k++) {
+						if(k==i || k==j) continue;
+						System.out.println(data[k]);
+					}
+					return;
+				}
+			}
+		}
+	}
+}
