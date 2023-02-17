2b7cd02e1259389f01249c793a97ab101bb0d1a2
dayounglee <dayoung015@gmail.com>
2023-02-17 20:43:52 +0900

    Update BOJ_2178.java
    조건 추가

--- BJ/BOJ_2178.java
+++ BJ/BOJ_2178.java
@@ -1,4 +1,5 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.Scanner;
 
@@ -25,19 +26,23 @@ public class BOJ_2178 {
 				board[i][j] = num;
 			}
 		}
-		bfs1();		
+		bfs();
 	}
 
-	private static void bfs1() {
+	private static void bfs() {
 		Queue<int[]> q = new ArrayDeque<>();
 		q.offer(new int[] {0,0});
 		visited[0][0] = true;
 		
 		int[] current;
 		int lvl = 0;
+		loop:
 		while(!q.isEmpty()) {
+			lvl++;
 			for(int i=0, size = q.size(); i<size; i++) {
 				current = q.poll();
+				
+				if(current[0] == N-1 && current[1] == M-1) break loop;
 				
 				for(int d=0; d<4; d++) {
 					int x = current[0]+dirR[d];
@@ -51,7 +56,6 @@ public class BOJ_2178 {
 					}
 				}
 			}
-			++lvl;
 		}
 		System.out.println(lvl);
 	}
